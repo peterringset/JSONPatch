b57e6e88aa6b5d99440b1faa80ae0aa7443ca7ec
peterringset <peter.ringset@eggsdesign.com>
2018-11-22 15:52:09 +0100

    Add initial Xcode project

---
+++ JSONPatchTests/JSONPatchTests.swift
@@ -0,0 +1,34 @@
+//
+//  JSONPatchTests.swift
+//  JSONPatchTests
+//
+//  Created by Peter Ringset on 22/11/2018.
+//  Copyright © 2018 Ringset. All rights reserved.
+//
+
+import XCTest
+@testable import JSONPatch
+
+class JSONPatchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
